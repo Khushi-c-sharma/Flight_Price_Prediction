a37ab1913a60c318066d598ca284168b9a14c05e
Khushi-c-Sharma <khushisharmalive1908@gmail.com>
2024-06-17 21:59:06 +0530

    add files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Flight price prediction - End - to - End Machine Learning project using AWS SageMaker
---
+++ web-app.py
@@ -0,0 +1,384 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import warnings
+import joblib
+import pickle
+import optuna
+import streamlit as st
+
+import sklearn
+import xgboost as xgb
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.ensemble import RandomForestRegressor
+from feature_engine.selection import SelectBySingleFeaturePerformance
+from feature_engine.datetime import DatetimeFeatures
+from feature_engine.outliers import Winsorizer
+from sklearn.preprocessing import MinMaxScaler, PowerTransformer, FunctionTransformer, StandardScaler
+from sklearn.compose import ColumnTransformer
+from feature_engine.encoding import RareLabelEncoder, MeanEncoder, CountFrequencyEncoder
+from sklearn.pipeline import FeatureUnion
+from sklearn.metrics.pairwise import rbf_kernel
+from sklearn.metrics import r2_score
+
+sklearn.set_config(transform_output="pandas")
+
+# preprocessing operations
+air_transformer = Pipeline(steps=[
+	("imputer", SimpleImputer(strategy="most_frequent")),
+	("grouper", RareLabelEncoder(tol=0.1, replace_with="Other", n_categories=2)),
+	("encoder", OneHotEncoder(sparse_output=False, handle_unknown="ignore"))
+])
+
+#doj
+feature_to_extract = ["month", "week", "day_of_week", "day_of_year"]
+
+doj_transformer = Pipeline(steps=[
+	("dt", DatetimeFeatures(features_to_extract=feature_to_extract, yearfirst=True, format="mixed")),
+	("scaler", MinMaxScaler())
+])
+
+# source & destination
+location_pipe1 = Pipeline(steps=[
+	("grouper", RareLabelEncoder(tol=0.1, replace_with="Other", n_categories=2)),
+	("encoder", MeanEncoder()),
+	("scaler", PowerTransformer())
+])
+
+def is_north(X):
+	columns = X.columns.to_list()
+	north_cities = ["Delhi", "Kolkata", "Mumbai", "New Delhi"]
+	return (
+		X
+		.assign(**{
+			f"{col}_is_north": X.loc[:, col].isin(north_cities).astype(int)
+			for col in columns
+		})
+		.drop(columns=columns)
+	)
+
+location_transformer = FeatureUnion(transformer_list=[
+	("part1", location_pipe1),
+	("part2", FunctionTransformer(func=is_north))
+])
+
+# dep_time & arrival_time
+time_pipe1 = Pipeline(steps=[
+	("dt", DatetimeFeatures(features_to_extract=["hour", "minute"])),
+	("scaler", MinMaxScaler())
+])
+
+def part_of_day(X, morning=4, noon=12, eve=16, night=20):
+	columns = X.columns.to_list()
+	X_temp = X.assign(**{
+		col: pd.to_datetime(X.loc[:, col], dayfirst=False, format="mixed").dt.hour
+		for col in columns
+	})
+
+	return (
+		X_temp
+		.assign(**{
+			f"{col}_part_of_day": np.select(
+				[X_temp.loc[:, col].between(morning, noon, inclusive="left"),
+				 X_temp.loc[:, col].between(noon, eve, inclusive="left"),
+				 X_temp.loc[:, col].between(eve, night, inclusive="left")],
+				["morning", "afternoon", "evening"],
+				default="night"
+			)
+			for col in columns
+		})
+		.drop(columns=columns)
+	)
+
+time_pipe2 = Pipeline(steps=[
+	("part", FunctionTransformer(func=part_of_day)),
+	("encoder", CountFrequencyEncoder()),
+	("scaler", MinMaxScaler())
+])
+
+time_transformer = FeatureUnion(transformer_list=[
+	("part1", time_pipe1),
+	("part2", time_pipe2)
+])
+
+# duration
+class RBFPercentileSimilarity(BaseEstimator, TransformerMixin):
+	def __init__(self, variables=None, percentiles=[0.25, 0.5, 0.75], gamma=0.1):
+		self.variables = variables
+		self.percentiles = percentiles
+		self.gamma = gamma
+
+
+	def fit(self, X, y=None):
+		if not self.variables:
+			self.variables = X.select_dtypes(include="number").columns.to_list()
+
+		self.reference_values_ = {
+			col: (
+				X
+				.loc[:, col]
+				.quantile(self.percentiles)
+				.values
+				.reshape(-1, 1)
+			)
+			for col in self.variables
+		}
+
+		return self
+
+
+	def transform(self, X):
+		objects = []
+		for col in self.variables:
+			columns = [f"{col}_rbf_{int(percentile * 100)}" for percentile in self.percentiles]
+			obj = pd.DataFrame(
+				data=rbf_kernel(X.loc[:, [col]], Y=self.reference_values_[col], gamma=self.gamma),
+				columns=columns
+			)
+			objects.append(obj)
+		return pd.concat(objects, axis=1)
+	
+
+def duration_category(X, short=180, med=400):
+	return (
+		X
+		.assign(duration_cat=np.select([X.duration.lt(short),
+										X.duration.between(short, med, inclusive="left")],
+									   ["short", "medium"],
+									   default="long"))
+		.drop(columns="duration")
+	)
+
+def is_over(X, value=1000):
+	return (
+		X
+		.assign(**{
+			f"duration_over_{value}": X.duration.ge(value).astype(int)
+		})
+		.drop(columns="duration")
+	)
+
+duration_pipe1 = Pipeline(steps=[
+	("rbf", RBFPercentileSimilarity()),
+	("scaler", PowerTransformer())
+])
+
+duration_pipe2 = Pipeline(steps=[
+	("cat", FunctionTransformer(func=duration_category)),
+	("encoder", OrdinalEncoder(categories=[["short", "medium", "long"]]))
+])
+
+duration_union = FeatureUnion(transformer_list=[
+	("part1", duration_pipe1),
+	("part2", duration_pipe2),
+	("part3", FunctionTransformer(func=is_over)),
+	("part4", StandardScaler())
+])
+
+duration_transformer = Pipeline(steps=[
+	("outliers", Winsorizer(capping_method="iqr", fold=1.5)),
+	("imputer", SimpleImputer(strategy="median")),
+	("union", duration_union)
+])
+
+# total_stops
+def is_direct(X):
+	return X.assign(is_direct_flight=X.total_stops.eq(0).astype(int))
+
+
+total_stops_transformer = Pipeline(steps=[
+	("imputer", SimpleImputer(strategy="most_frequent")),
+	("", FunctionTransformer(func=is_direct))
+])
+
+# additional_info
+info_pipe1 = Pipeline(steps=[
+	("group", RareLabelEncoder(tol=0.1, n_categories=2, replace_with="Other")),
+	("encoder", OneHotEncoder(handle_unknown="ignore", sparse_output=False))
+])
+
+def have_info(X):
+	return X.assign(additional_info=X.additional_info.ne("No Info").astype(int))
+
+info_union = FeatureUnion(transformer_list=[
+	("part1", info_pipe1),
+	("part2", FunctionTransformer(func=have_info))
+])
+
+info_transformer = Pipeline(steps=[
+	("imputer", SimpleImputer(strategy="constant", fill_value="unknown")),
+	("union", info_union)
+])
+
+# column transformer
+column_transformer = ColumnTransformer(transformers=[
+	("air", air_transformer, ["airline"]),
+	("doj", doj_transformer, ["date_of_journey"]),
+	("location", location_transformer, ["source", 'destination']),
+	("time", time_transformer, ["dep_time", "arrival_time"]),
+	("dur", duration_transformer, ["duration"]),
+	("stops", total_stops_transformer, ["total_stops"]),
+	("info", info_transformer, ["additional_info"])
+], remainder="passthrough")
+
+# feature selector
+estimator = RandomForestRegressor(n_estimators=10, max_depth=3, random_state=42)
+
+selector = SelectBySingleFeaturePerformance(
+	estimator=estimator,
+	scoring="r2",
+	threshold=0.1
+) 
+
+# preprocessor
+preprocessor = Pipeline(steps=[
+	("ct", column_transformer),
+	("selector", selector)
+])
+
+# Reading the training data
+file_path = r"C:\Users\User\OneDrive\Desktop\Flight-Price-Prediction\Data\train.csv"
+
+train = pd.read_csv(file_path)
+
+X_train = train.drop(columns="price")
+y_train = train.price.copy()
+
+# Fit and save the preprocessor
+preprocessor.fit(X_train, y_train)
+
+joblib.dump(preprocessor, "preprocessor.joblib")
+
+
+# # Web application
+
+# In[2]:
+
+
+st.set_page_config(
+    page_title = "Flight Prices Prediction",
+    page_icon = "✈️",
+    layout = "wide"
+)
+
+
+# In[3]:
+
+
+st.title("Flight Prices Prediction")
+
+
+# In[4]:
+
+
+airline = st.selectbox("Airline:",
+                      options=X_train.airline.unique())
+
+
+# In[5]:
+
+
+doj = st.date_input("Date of Journey : ")
+
+
+# In[6]:
+
+
+src = st.selectbox("Source:",
+                      options=X_train.source.unique())
+
+
+# In[7]:
+
+
+destination = st.selectbox("Destination:",
+                      options=X_train.destination.unique())
+
+
+# In[8]:
+
+
+dep_time = st.time_input("Departure time : ")
+
+
+# In[9]:
+
+
+arrival_time = st.time_input("Arrival time : ")
+
+
+# In[10]:
+
+
+duration = st.number_input(
+    "Duration (minutes):",
+    step=1
+)
+
+
+# In[11]:
+
+
+total_stops = st.number_input(
+    "Total stops :",
+    step=1,
+    min_value = 0
+)
+
+
+# In[12]:
+
+
+additional_info = st.selectbox(
+    "Additional information : ",
+    options=X_train.additional_info.unique()
+)
+
+
+# In[13]:
+
+
+x_new = pd.DataFrame(dict(
+    airline=[airline],
+    date_of_journey=[doj],
+    source=[src],
+    destination=[destination],
+    dep_time=[dep_time],
+    arrival_time=[arrival_time],
+    duration=[duration],
+    total_stops=[total_stops],
+    additional_info=[additional_info]
+)).astype({
+    col : "str"
+    for col in ["date_of_journey", "dep_time", "arrival_time"]
+})
+
+
+# In[15]:
+
+
+if st.button("Predict"):
+    saved_preprocessor = joblib.load("preprocessor.joblib")
+    x_new_transformed = saved_preprocessor.transform(x_new)
+    
+    with open("xgboost-best-model.pkl", "rb") as f:
+        model = pickle.load(f)
+        
+    x_new_xgb = xgb.DMatrix(x_new_transformed)
+    pred = model.predict(x_new_xgb)
+    st.info(f"The predicted price is {pred[0]} INR")
+
+
+# In[ ]:
+
+
+
+
